386becdde3d03ba125a6cca4aa31e873c68f39fc
ZacheryYoung <awdzh888@outlook.com>
2017-12-24 14:09:10 +0800

    Initial commit

---
+++ src/test/java/testTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class testTest {
+
+    @Test
+    public void main() {
+        test te = new test();
+        String st = te.test();
+        String str = "Hello!";
+        assertEquals(st, str);
+    }
+}
